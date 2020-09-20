8f7ce045f31cc7aa94d9ce09f7a4dd25c57fb800
Paweł Sosnowiec <pawel@boosttypers.com>
2020-09-20 11:18:21 +0200

    added database connection and save weather to database

--- src/Controller/api/ApiController.php
+++ src/Controller/api/ApiController.php
@@ -4,27 +4,44 @@
 namespace App\Controller\api;
 
 
+use App\Entity\Weather;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class ApiController extends AbstractController
 {
     private $client;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+    private $weatherRepository;
     private $apiKey = '2782634eed1c90284914691197d5acc4';
 
-    public function __construct(HttpClientInterface $client)
+    public function __construct(HttpClientInterface $client, EntityManagerInterface $em)
     {
         $this->client = $client;
+        $this->em = $em;
+        $this->weatherRepository = $this->em->getRepository(Weather::class);
     }
 
     /**
      * @param Request $request
-     * @return \App\Controller\api\JsonResponse
+     * @return JsonResponse
      * @throws TransportExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ServerExceptionInterface
      * @Route("/api/get-weather", name="get_weather", methods={"POST", "GET"})
      */
     public function getAction(Request $request)
@@ -34,19 +51,40 @@ class ApiController extends AbstractController
             'query' => [
                 'lat' => $request->request->get('lat'),
                 'lon' => $request->request->get('lng'),
-                'appid' => '2782634eed1c90284914691197d5acc4',
+                'appid' => $this->apiKey,
             ],
         ]);
         $response = $response->toArray();
 
-         $this->saveWeather($response);
-
-        return new JsonResponse(['status' => 'success', 'message' => 'Pogoda została zapisana'], 200);
+         if($this->saveWeather($response)){
+             return new JsonResponse(['status' => 'success', 'message' => 'Pogoda została zapisana'], 200);
+         } else {
+             return new JsonResponse(['status' => 'error', 'message' => 'Coś poszło nie tak!'], 500);
+         }
     }
 
     public function saveWeather($response)
     {
-//        $this->k_to_c($response['temp']);
+
+        if ($response !== null){
+            $weather = new Weather();
+
+            $temp = $this->k_to_c($response['main']['temp']);
+            $weather->setCity($response['name'])
+                ->setLat($response['coord']['lat'])
+                ->setLng($response['coord']['lon'])
+                ->setWindSpeed($response['wind']['speed'])
+                ->setVisibility($response['visibility'])
+                ->setDescription($response['weather'][0]['description'])
+                ->setMainWeather($response['weather'][0]['main'])
+                ->setTemp($temp);
+            $this->em->persist($weather);
+            $this->em->flush();
+
+            return true;
+        }else {
+            return false;
+        }
     }
 
     private function k_to_c($temp) {
